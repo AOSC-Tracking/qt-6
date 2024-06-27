595718878da2fb11a3fde6785a274bcf4611ffdf
Martin Negyokru <negyokru@inf.u-szeged.hu>
2024-06-27 12:29:29 +0200

    Fixup: Fix building with system ffmpeg
    Add support for FFmpeg 7 and 6.1.
    FFmpeg 6.0 and below need bigger changes.
    
    Cleanup old code, use guards instead of direct changes.
    
    Fixes: QTBUG-125227
    Change-Id: I6c8293f5eb8b83de01977a504801b013571f7c2b
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-src/3rdparty/chromium/+/575610
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- qtwebengine/src/3rdparty/chromium/media/filters/ffmpeg_demuxer.cc
+++ qtwebengine/src/3rdparty/chromium/media/filters/ffmpeg_demuxer.cc
@@ -46,10 +46,8 @@
 #include "media/formats/mpeg/mpeg1_audio_stream_parser.h"
 #include "media/formats/webm/webm_crypto_helpers.h"
 #include "media/media_buildflags.h"
-#if !BUILDFLAG(USE_SYSTEM_FFMPEG)
 #include "third_party/ffmpeg/ffmpeg_features.h"
 #include "third_party/ffmpeg/libavcodec/packet.h"
-#endif  // !BUILDFLAG(USE_SYSTEM_FFMPEG)
 
 #if BUILDFLAG(ENABLE_PLATFORM_HEVC)
 #include "media/filters/ffmpeg_h265_to_annex_b_bitstream_converter.h"
@@ -77,11 +75,6 @@ int AVSeekFrame(AVFormatContext* s, int stream_index, int64_t timestamp) {
   return av_seek_frame(s, stream_index, timestamp, 0);
 }
 
-#if LIBAVCODEC_VERSION_MAJOR < 59
-auto av_stream_get_first_dts(AVStream* stream) {
-  return stream->first_dts;
-}
-#endif
 }  // namespace
 
 static base::Time ExtractTimelineOffset(
@@ -401,16 +394,11 @@ void FFmpegDemuxerStream::EnqueuePacket(ScopedAVPacket packet) {
 
   scoped_refptr<DecoderBuffer> buffer;
 
-#if LIBAVCODEC_VERSION_MAJOR < 59
-  typedef int side_data_arg;
-#else
-  typedef size_t side_data_arg;
-#endif
   if (type() == DemuxerStream::TEXT) {
     // TODO(crbug.com/1471504): This is now broken without side data; remove.
     buffer = DecoderBuffer::CopyFrom(packet->data, packet->size);
   } else {
-    side_data_arg side_data_size = 0;
+    size_t side_data_size = 0;
     uint8_t* side_data = av_packet_get_side_data(
         packet.get(), AV_PKT_DATA_MATROSKA_BLOCKADDITIONAL, &side_data_size);
 
@@ -473,7 +461,7 @@ void FFmpegDemuxerStream::EnqueuePacket(ScopedAVPacket packet) {
                                        packet->size - data_offset);
     }
 
-    side_data_arg skip_samples_size = 0;
+    size_t skip_samples_size = 0;
     const uint32_t* skip_samples_ptr =
         reinterpret_cast<const uint32_t*>(av_packet_get_side_data(
             packet.get(), AV_PKT_DATA_SKIP_SAMPLES, &skip_samples_size));
